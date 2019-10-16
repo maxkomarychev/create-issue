6cf30de00c1dd61d3999eb93af1bf8721d79ebc4
Max Komarychev <maxkomarychev@gmail.com>
2019-10-16 22:57:17 +0300

    Deploy v0.4.0

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# create-issue
+# Create issue
+
+Create issue
+
+This action is a wrapper for one of [octokit's](https://octokit.github.io/rest.js) methods.
+
+Original docs can be found here: https://octokit.github.io/rest.js/#octokit-routes-issues-create
+
+# Usage
+
+```yaml
+- uses: maxkomarychev/create-issue@v0.4.0
+  id: my_step_id
+  with:
+    token: ${{ secrets.GITHUB_TOKEN }}
+    title: Hello, World!
+- name: Print outputs
+  run: |
+    echo ${{ steps.my_step_id.outputs.id }}
+    echo ${{ steps.my_step_id.outputs.number }}
+```
---
+++ index.js
@@ -0,0 +1,60 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function parse_array(input_name) {
+  const input_value = core.getInput(input_name)
+  if (input_value === "") {
+    return undefined; 
+  }
+  if (input_value === "<<EMPTY>>") {
+    return [];
+  }
+  return input_value.split(",");
+}
+
+function parse_boolean(input_name) {
+  const input_value = core.getInput(input_name)
+  if (!input_name) {
+    return false
+  }
+  return input_name === "true"
+}
+
+function default_parse(input_name) {
+  const input_value = core.getInput(input_name)
+  return input_value || undefined
+}
+
+try {
+  const token = default_parse("token");
+  const title = default_parse("title");
+  const body = default_parse("body");
+  const milestone = default_parse("milestone");
+  const labels = parse_array("labels");
+  const assignees = parse_array("assignees");
+  const client = new github.GitHub(token);
+  const context = github.context;
+  client.issues.create({
+    ...context.repo,
+      token,
+      title,
+      body,
+      milestone,
+      labels,
+      assignees,
+    headers: {
+      "Accept": "application/vnd.github.flash-preview+json, application/vnd.github.ant-man-preview+json",
+    }
+  }).then(response => {
+    console.log('response', response)
+    core.setOutput("id", response.data.id)
+    core.setOutput("number", response.data.number)
+  })
+  .catch(error => {
+    console.log("error 1", error);
+    core.setFailed(error.message);
+  });
+} catch (error) {
+  console.log("error 2", error);
+  core.setFailed(error.message);
+}
